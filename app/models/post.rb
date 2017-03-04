e1fd060121b0e76258f10d9118c47771b0603757
hilaryml <hilyttle@gmail.com>
2017-03-04 16:08:04 -0500

    Adds user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :image, presence: true
+  has_attached_file :image, styles: { :medium => "400x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_secure_password
+  validates :username, :email, :password, presence: true
 end
