4990642de536fcc88d402014f7c57eae3ce9594f
hilaryml <hilyttle@gmail.com>
2017-03-06 15:48:49 -0500

    Adds crud to users_controller.js

--- app/assets/javascripts/angular/controllers/users_controller.js
+++ app/assets/javascripts/angular/controllers/users_controller.js
@@ -1,5 +1,22 @@
 angular.module('app', ['ui.router', 'templates'])
 
-  .controller('UsersController', function($scope) {
-    
+  .controller('UsersController', function ($scope, UserService, $location) {
+    //callback for ng-click createUser
+    $scope.createUser = function () {
+      $location.path('/users/new');
+    }
+
+    //callback for ng-click editUser
+    $scope.editUser = function (userId) {
+      $location.path('users/edit' + userId);
+      $scope.users = UserService.query();
+    }
+
+    //callback for ng-click deleteUser
+    $scope.deleteUser = function (userId) {
+      UserService.delete({ id: userId });
+      $scope.users = UserService.query();
+    }
+
+    $scope.users = UserService.query();
   });
