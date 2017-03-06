f59a00ad2cf5440f4b344afb983f154864a75926
hilaryml <hilyttle@gmail.com>
2017-03-06 15:09:55 -0500

    Updates application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  after_filter  :set_csrf_cookie_for_ng
+
+  def current_user
+    session[:user_id] ||= User.new
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
 end
