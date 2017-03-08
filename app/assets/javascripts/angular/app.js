8d100ad376a19f495209767530b580f07b419a4e
hilaryml <hilyttle@gmail.com>
2017-03-07 20:35:24 -0500

    Updates signup signin and home templates, minor changes to states in app.js

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -7,12 +7,12 @@ angular.module('app', ['templates', 'ui.router', 'ngMessages'])
         templateUrl: 'angular/templates/application/home.html',
         controller: 'UsersController as user'
       })
-      .state('home.signin', {
+      .state('signin', {
         url: '/signin',
         templateUrl: 'angular/templates/users/signin.html',
         controller: 'UsersController as user'
       })
-      .state('home.signup', {
+      .state('signup', {
         url: '/signup',
         templateUrl: 'angular/templates/users/signup.html',
         controller: 'UsersController as user'
