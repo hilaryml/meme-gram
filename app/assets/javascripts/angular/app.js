d76c8c609eb294a17c67fb1eb666b26f09d4afa5
hilaryml <hilyttle@gmail.com>
2017-03-08 22:23:37 -0500

    Minor changes to bower.json file (removes angular resources), app.js, and users signin template

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,22 +1,22 @@
 angular.module('app', ['templates', 'ui.router', 'ngMessages'])
 
-  .config(function($stateProvider, $urlRouterProvider) {
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'angular/templates/application/home.html',
-        controller: 'UsersController as user'
+        controller: 'UsersController as ctrl'
       })
       .state('signin', {
         url: '/signin',
         templateUrl: 'angular/templates/users/signin.html',
-        controller: 'UsersController as user'
+        controller: 'UsersController as ctrl'
       })
       .state('signup', {
         url: '/signup',
         templateUrl: 'angular/templates/users/signup.html',
-        controller: 'UsersController as user'
+        controller: 'UsersController as ctrl'
       })
 
   $urlRouterProvider.otherwise('/');
-});
+}]);
