757b247993bd9aaa0a34a9e93ca7303197f69cfb
hilaryml <hilyttle@gmail.com>
2017-03-15 12:17:16 -0400

    Gets rid of duplicate csrf code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  after_action  :set_csrf_cookie_for_ng
-
   def index
   end
 
@@ -14,13 +12,4 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
-
-  protected
-
-  def verified_request?
-    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
-  end
 end
